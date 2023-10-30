a1e9cf31f8ba4f0da367b3c0854a5b7164ca367f
skillz <skillzdev@hotmail.com>
2023-10-30 11:06:25 +0100

    added controllers and auth

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+
+class RegisterController extends Controller
+{
+    public function showRegisterForm(): View
+    {
+        return view('auth.register');
+        // TODO TO CREATE
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'role' => 'required|string',
+            // TODO TO MODIFY AND ADD ATTRIBUTES OF DELIVERYMAN OR ARTISAN
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:8|confirmed',
+            'password_confirmation' => 'required|string|min:8',
+        ]);
+
+        $data['password'] = bcrypt($data['password']);
+        if ($data['role'] === 'artisan') {
+            $creation = User::create($data);
+            // TODO CREATE ARTISAN
+        } else if ($data['role'] === 'delivery_man') {
+            $user = User::create($data);
+            // TODO CREATE DELIVERY MAN
+        } else {
+            $user = User::create($data);
+        }
+
+        auth()->login($user);
+
+        return redirect()->route('index');
+        // TODO CREATE INDEX PAGE
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <x-default-layout>
 
     <div class="h-96">
-        <h1 class="text-2xl font-bold">Hello</h1>
     </div>
 </x-default-layout>
