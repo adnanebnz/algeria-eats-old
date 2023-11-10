837e95b4a59bb103d24dcaf76f2428fd3cdffdd3
skillz <skillzdev@hotmail.com>
2023-11-10 21:59:28 +0100

    edited cart funcs

--- app/Livewire/CartComponent.php
+++ app/Livewire/CartComponent.php
@@ -7,18 +7,19 @@ use Livewire\Component;
 
 class CartComponent extends Component
 {
-    protected $listeners = ['itemAdded' => 'render', 'itemRemoved' => 'render'];
+    public $cartCount;
+
+    protected $listeners = ['cartAddedUpdated' => 'render'];
 
     public function remove($id)
     {
         Cart::where('product_id', $id)->where('user_id', auth()->user()->id)->delete();
-        $this->dispatch('itemRemoved');
+        $this->dispatch('cartAddedUpdated');
     }
 
     public function render()
     {
-        $cartItems = Cart::where('user_id', auth()->user()->id)->get();
-        $cartCount = Cart::where('user_id', auth()->user()->id)->count();
-        return view('livewire.cart-component', ['cartItems' =>  $cartItems, 'cartCount' => $cartCount]);
+        $this->cartCount = Cart::where('user_id', auth()->user()->id)->count();
+        return view('livewire.cart-component', ['cartCount' => $this->cartCount]);
     }
 }
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -1,15 +1,15 @@
 <div>
     {{-- TODO CART START --}}
     @auth
-        <div x-data="{ isCartOpen: false }" x-cloak>
+        <div>
             <div class="flex justify-center">
                 <div class="relative">
                     <div class="flex flex-row cursor-pointer truncate p-2 px-4 rounded">
-                        <div></div>
                         <div class="flex flex-row-reverse ml-2 w-full">
-                            <div slot="icon" class="relative" @click="isCartOpen=!isCartOpen">
+                            <div slot="icon" class="relative">
+                                {{-- TODO CREATE CART PAGE AND REDIRECT TO IT --}}
                                 <div
-                                    class="absolute text-xs rounded-full -mt-1 -mr-2 px-1 font-bold top-0 right-0 bg-red-700 text-white">
+                                    class="absolute text-xs rounded-full -mt-1 -mr-2 px-1 font-bold top-0 right-0 bg-red-500 text-white">
                                     {{ $cartCount }}</div>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -21,56 +21,8 @@
                             </div>
                         </div>
                     </div>
-                    <div x-show="isCartOpen" class="absolute w-full rounded-b border-t-0 z-30">
-                        <div class="shadow-xl w-64">
-                            @if ($cartCount > 0)
-                                <div>
-                                    @foreach ($cartItems as $item)
-                                        <div
-                                            class="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100">
-                                            <div class="p-2 w-12"><img src="{{ $item->product->images[0] }}" alt="image">
-                                            </div>
-                                            <div class="flex-auto text-sm w-32">
-                                                <div class="font-bold">{{ $item->product->nom }}</div>
-                                                <div class="truncate">{{ $item->product->description }}</div>
-                                                <div class="text-gray-400">Qt: {{ '1' }}</div>
-                                            </div>
-                                            <div class="flex flex-col w-18 font-medium items-end">
-                                                <button type="button" wire:click="remove('{{ $item->product->id }}')"
-                                                    class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
-                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                        class="feather feather-trash-2">
-                                                        <polyline points="3 6 5 6 21 6"></polyline>
-                                                        <path
-                                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                                        </path>
-                                                        <line x1="10" y1="11" x2="10" y2="17">
-                                                        </line>
-                                                        <line x1="14" y1="11" x2="14" y2="17">
-                                                        </line>
-                                                    </svg>
-                                                </button>
-                                                {{ $item->product->prix }} DA
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                </div>
-                                <div class="p-4
-        justify-center flex bg-white">
-                                    <button
-                                        class="text-base hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded-md font-bold cursor-pointer border duration-200 ease-in-out border-blue-600 transition">Checkout</button>
-                                    {{-- todo calculate max price and show it here --}}
-                                </div>
-                            @else
-                                <p class="p-4">Cart is empty</p>
-                            @endif
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
     @endauth
-    {{-- todo end cart --}}
 </div>
