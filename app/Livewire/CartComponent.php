85b85c0b77cde004f4a454d45ac2b1eccd3e2f1d
skillz <skillzdev@hotmail.com>
2023-11-10 21:26:20 +0100

    changed products to be a livewire component and added cart funcs

--- app/Livewire/CartComponent.php
+++ app/Livewire/CartComponent.php
@@ -2,20 +2,23 @@
 
 namespace App\Livewire;
 
-use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Models\Cart;
 use Livewire\Component;
 
 class CartComponent extends Component
 {
+    protected $listeners = ['itemAdded' => 'render', 'itemRemoved' => 'render'];
 
-    public function remove($rowId)
+    public function remove($id)
     {
-        Cart::remove($rowId);
-        return redirect()->route('product.index');
+        Cart::where('product_id', $id)->where('user_id', auth()->user()->id)->delete();
+        $this->dispatch('itemRemoved');
     }
 
     public function render()
     {
-        return view('livewire.cart-component');
+        $cartItems = Cart::where('user_id', auth()->user()->id)->get();
+        $cartCount = Cart::where('user_id', auth()->user()->id)->count();
+        return view('livewire.cart-component', ['cartItems' =>  $cartItems, 'cartCount' => $cartCount]);
     }
 }
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -10,7 +10,7 @@
                             <div slot="icon" class="relative" @click="isCartOpen=!isCartOpen">
                                 <div
                                     class="absolute text-xs rounded-full -mt-1 -mr-2 px-1 font-bold top-0 right-0 bg-red-700 text-white">
-                                    {{ Cart::count() }}</div>
+                                    {{ $cartCount }}</div>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                     stroke-linejoin="round" class="feather feather-shopping-cart w-6 h-6 mt-2">
@@ -23,40 +23,40 @@
                     </div>
                     <div x-show="isCartOpen" class="absolute w-full rounded-b border-t-0 z-30">
                         <div class="shadow-xl w-64">
-                            {{-- TODO CART CONTENT HERE --}}
-                            @if (Cart::count() > 0)
-                                @foreach (Cart::content() as $item)
-                                    <div
-                                        class="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100">
-                                        <div class="p-2 w-12"><img src="{{ $item->model->images[0] }}" alt="image"></div>
-                                        <div class="flex-auto text-sm w-32">
-                                            <div class="font-bold">{{ $item->model->nom }}</div>
-                                            <div class="truncate">{{ $item->model->description }}</div>
-                                            <div class="text-gray-400">Qt: {{ '1' }}</div>
+                            @if ($cartCount > 0)
+                                <div>
+                                    @foreach ($cartItems as $item)
+                                        <div
+                                            class="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100">
+                                            <div class="p-2 w-12"><img src="{{ $item->product->images[0] }}" alt="image">
+                                            </div>
+                                            <div class="flex-auto text-sm w-32">
+                                                <div class="font-bold">{{ $item->product->nom }}</div>
+                                                <div class="truncate">{{ $item->product->description }}</div>
+                                                <div class="text-gray-400">Qt: {{ '1' }}</div>
+                                            </div>
+                                            <div class="flex flex-col w-18 font-medium items-end">
+                                                <button type="button" wire:click="remove('{{ $item->product->id }}')"
+                                                    class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-trash-2">
+                                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                                        <path
+                                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                        </path>
+                                                        <line x1="10" y1="11" x2="10" y2="17">
+                                                        </line>
+                                                        <line x1="14" y1="11" x2="14" y2="17">
+                                                        </line>
+                                                    </svg>
+                                                </button>
+                                                {{ $item->product->prix }} DA
+                                            </div>
                                         </div>
-                                        <div class="flex flex-col w-18 font-medium items-end">
-                                            <a href="#" wire:click.prevent="remove('{{ $item->rowId }}')"
-                                                class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
-                                                    fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                    class="feather feather-trash-2">
-                                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                                    <path
-                                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                                    </path>
-                                                    <line x1="10" y1="11" x2="10" y2="17">
-                                                    </line>
-                                                    <line x1="14" y1="11" x2="14" y2="17">
-                                                    </line>
-                                                </svg>
-                                            </a>
-                                            {{ $item->model->prix }} DA
-                                        </div>
-                                    </div>
-                                @endforeach
-
-                                {{-- TODO CART CONTENT FINISH --}}
+                                    @endforeach
+                                </div>
                                 <div class="p-4
         justify-center flex bg-white">
                                     <button
