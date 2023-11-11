017f3251804fb9a51fea3d284653757ec6b8aa85
skillz <skillzdev@hotmail.com>
2023-11-11 13:38:29 +0100

    setted up todo

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -1,15 +1,14 @@
 <div>
     <main>
-        <div class="h-screen">
+        <div class="h-screen mb-10">
             <h1 class="mb-10 text-center text-2xl font-bold">Panier</h1>
             <div class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
                 <div class="rounded-lg md:w-2/3">
                     @foreach ($cartItems as $cartItem)
                         <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start"
                             wire:key='{{ $cartItem->id }}'>
-                            <img src="{{ $cartItem->product->images[0] }}
-                                alt="product-image"
-                                class="w-full rounded-lg sm:w-40" />
+                            <img src="{{ str_starts_with($cartItem->product->images[0], 'http') ? $cartItem->product->images[0] : asset('storage/' . $cartItem->product->images[0]) }}"
+                                class="w-20 rounded-lg" />
                             <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                                 <div class="mt-5 sm:mt-0">
                                     <h2 class="text-lg font-bold text-gray-900">{{ $cartItem->product->nom }}</h2>
@@ -19,14 +18,14 @@
                                 </div>
                                 <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                                     <div class="flex items-center border-gray-100">
-                                        <span
+                                        <button
                                             class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
-                                            - </span>
+                                            - </button>
                                         <input class="h-8 w-8 border bg-white text-center text-xs outline-none"
-                                            type="number" value="1" min="1" />
-                                        <span
+                                            type="number" value="{{ $cartItem->quantity }}" min="1" />
+                                        <button
                                             class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
-                                            + </span>
+                                            + </button>
                                     </div>
                                     <div class="flex items-center space-x-4">
                                         <p class="text-sm">{{ $cartItem->product->prix }} DA </p>
