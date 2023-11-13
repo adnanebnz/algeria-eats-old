81480275399396569042df6231f0ba456b83d25a
skillz <skillzdev@hotmail.com>
2023-11-13 16:48:18 +0100

    creted alert component and modified SInglePoduct and Prodct Component to redirect to the desired product according to its ID and deleted route key name

---
+++ app/Livewire/FeedbackMessage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class FeedbackMessage extends Component
+{
+    public $message;
+    public $type;
+
+    public function mount($message, $type = 'success')
+    {
+        $this->message = $message;
+        $this->type = $type;
+    }
+
+    public function render()
+    {
+        return view('livewire.feedback-message');
+    }
+}
--- app/Livewire/SingleProduct.php
+++ app/Livewire/SingleProduct.php
@@ -2,16 +2,45 @@
 
 namespace App\Livewire;
 
+use App\Models\Cart;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class SingleProduct extends Component
 {
     public $product;
+    public $quantity = 1;
+    public $feedbackMessage;
+    public $feedbackMessageType;
+    public $selectedImage = 0;
+
 
     public function mount(Product $product)
     {
         $this->product = $product;
+    }
+
+    public function addToCart()
+    {
+        if (Auth::check()) {
+            if (Cart::where('product_id', $this->product->id)->where('user_id', auth()->user()->id)->exists()) {
+                $this->feedbackMessage = 'Produit déja dans le panier';
+                $this->feedbackMessageType = 'error';
+            } else {
+                Cart::create([
+                    'product_id' => $this->product->id,
+                    'user_id' => auth()->user()->id,
+                    'quantity' => 1
+                ]);
+                $this->dispatch('cartAddedUpdated');
+                $this->quantity = 1;
+                $this->feedbackMessage = 'Produit ajouté au panier';
+                $this->feedbackMessageType = 'success';
+            }
+        } else {
+            return redirect()->route('login');
+        }
     }
 
     public function render()
