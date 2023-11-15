acd560ac3e4d32b1735b17cf882eef8b07d5ddfe
skillz <skillzdev@hotmail.com>
2023-11-15 18:58:53 +0100

    added order functionalities

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use AnouarTouati\AlgerianCitiesLaravel\Facades\AlgerianCitiesFacade;
 use App\Models\Cart;
 use App\Models\Order;
 use Illuminate\Http\Request;
@@ -16,41 +17,56 @@ class OrderController extends Controller
 
     public function checkout()
     {
-        $wilayasData = require storage_path('cities.php');
-
-        $filteredData = array_reduce($wilayasData, function ($carry, $item) {
-            $key = $item['wilaya_code'];
-
-            if (!isset($carry[$key])) {
-                $carry[$key] = $item['wilaya_name_ascii'];
-            }
-
-            return $carry;
-        }, []);
-
-        $cartItems = Cart::where('user_id', auth()->user()->id)->get();
         $total = 0;
+        $wilayas = AlgerianCitiesFacade::getAllWilayas();
+        $cartItems = Cart::where('user_id', auth()->user()->id)->get();
         foreach ($cartItems as $cartItem) {
             $total += $cartItem->product->prix * $cartItem->quantity;
         }
-        return view('cart.checkout', compact('cartItems', 'total', 'filteredData'));
+
+        return view('cart.checkout', compact('wilayas', 'total', 'cartItems'));
     }
 
     public function store(Request $request)
     {
         $data = $request->validate([
-            'consumer_id' => 'required|integer',
-            'artisan_id' => 'required|integer',
-            'product_id' => 'required|integer',
-            'quantity' => 'required|integer',
-            'prix_total' => 'required|integer',
-            'adresse' => 'required|string',
-            'phone' => 'required|string',
+            'email' => 'required|email',
+            'num_telephone' => 'required|numeric',
+            'adresse' => 'required',
+            'wilaya_name_ascii' => 'required',
         ]);
 
-        Order::create($data);
+        $cartItems = Cart::where('user_id', auth()->user()->id)->get();
+
+        foreach ($cartItems as $cartItem) {
+            $data['artisan_id'] = $cartItem->product->artisan_id;
+            $data['product_id'] = $cartItem->product_id;
+            $data['quantity'] = $cartItem->quantity;
+            $data['total'] = $cartItem->product->prix * $cartItem->quantity;
+
+            Order::create([
+                'consumer_id' => auth()->user()->id,
+                'status' => 'pending',
+                'artisan_id' => $data['artisan_id'],
+                'product_id' => $data['product_id'],
+                'prix_total' => $data['total'],
+                'quantity' => $data['quantity'],
+                'wilaya' => $data['wilaya_name_ascii'],
+                'email' => $data['email'],
+                'num_telephone' => $data['num_telephone'],
+                'adresse' => $data['adresse'],
+            ]);
+            // DELETE CART ITEMS
+            $cartItem->delete();
+        }
         Alert::success('Succès', 'Votre commande a été enregistrée avec succès');
-        return redirect()->route('artisan.index');
+        return redirect()->route('index');
         // TODO CREATE SUCCESS PAGE
+    }
+
+    public function cancel()
+    {
+        Alert::error('Erreur', 'Votre commande a été annulée');
+        return redirect()->route('index');
     }
 }
