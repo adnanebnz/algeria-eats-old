6473fca71c32db24c092860d22bfc4becb696a3b
skillz <skillzdev@hotmail.com>
2023-11-15 15:38:12 +0100

    added algeria cities

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -97,6 +97,9 @@
                     <select type="text" name="ville"
                         class="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500">
                         <option value="State">Ville</option>
+                        @foreach ($filteredData as $wilayaCode => $wilayaName)
+                            <option value="{{ $wilayaCode }}">{{ $wilayaCode }} - {{ $wilayaName }}</option>
+                        @endforeach
                     </select>
                     <input type="text" name="code_postale"
                         class="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
