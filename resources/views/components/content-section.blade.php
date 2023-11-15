3de5b6448343bbe30f1dd0dc21972d091547850d
skillz <skillzdev@hotmail.com>
2023-11-15 22:56:28 +0100

    modified components

--- resources/views/components/content-section.blade.php
+++ resources/views/components/content-section.blade.php
@@ -22,7 +22,7 @@
             class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div class="lg:pr-4">
                 <div class="lg:max-w-lg">
-                    <p class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
+                    <p class="text-base font-semibold leading-7 text-blue-600">Deploy faster</p>
                     <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
                     <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper
                         neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat
@@ -45,7 +45,7 @@
                         enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
                     <ul role="list" class="mt-8 space-y-8 text-gray-600">
                         <li class="flex gap-x-3">
-                            <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor"
+                            <svg class="mt-1 h-5 w-5 flex-none text-blue-600" viewBox="0 0 20 20" fill="currentColor"
                                 aria-hidden="true">
                                 <path fill-rule="evenodd"
                                     d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
@@ -56,7 +56,7 @@
                                 iste dolor cupiditate blanditiis ratione.</span>
                         </li>
                         <li class="flex gap-x-3">
-                            <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor"
+                            <svg class="mt-1 h-5 w-5 flex-none text-blue-600" viewBox="0 0 20 20" fill="currentColor"
                                 aria-hidden="true">
                                 <path fill-rule="evenodd"
                                     d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
@@ -66,7 +66,7 @@
                                 magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
                         </li>
                         <li class="flex gap-x-3">
-                            <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor"
+                            <svg class="mt-1 h-5 w-5 flex-none text-blue-600" viewBox="0 0 20 20" fill="currentColor"
                                 aria-hidden="true">
                                 <path
                                     d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
