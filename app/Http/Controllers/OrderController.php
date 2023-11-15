3219dd522dd54ea7f38d8782751ac8ade6ffab9b
skillz <skillzdev@hotmail.com>
2023-11-15 15:38:12 +0100

    added algeria cities

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,12 +16,24 @@ class OrderController extends Controller
 
     public function checkout()
     {
+        $wilayasData = require storage_path('cities.php');
+
+        $filteredData = array_reduce($wilayasData, function ($carry, $item) {
+            $key = $item['wilaya_code'];
+
+            if (!isset($carry[$key])) {
+                $carry[$key] = $item['wilaya_name_ascii'];
+            }
+
+            return $carry;
+        }, []);
+
         $cartItems = Cart::where('user_id', auth()->user()->id)->get();
         $total = 0;
         foreach ($cartItems as $cartItem) {
             $total += $cartItem->product->prix * $cartItem->quantity;
         }
-        return view('cart.checkout', compact('cartItems', 'total'));
+        return view('cart.checkout', compact('cartItems', 'total', 'filteredData'));
     }
 
     public function store(Request $request)
