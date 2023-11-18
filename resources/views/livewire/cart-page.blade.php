c2d25e1578daddd9ca8ef34f3b52527f8884a893
skillz <skillzdev@hotmail.com>
2023-11-18 15:05:09 +0100

    modified cart style

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -68,14 +68,14 @@
                 @if ($cartItems->count() > 0)
                     <!-- Sub total -->
                     <div class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-                        <div class="mb-2 flex justify-between">
-                            <p class="text-gray-700">Subtotal</p>
-                            <p class="text-gray-700">$129.99</p>
-                        </div>
-                        <div class="flex justify-between">
-                            <p class="text-gray-700">Shipping</p>
-                            <p class="text-gray-700">$4.99</p>
-                        </div>
+                        @foreach ($cartItems as $cartItem)
+                            <div class="mb-2 flex justify-between">
+                                <p class="text-gray-700">{{ $cartItem->product->nom }} &times;
+                                    {{ $cartItem->quantity }}</p>
+                                <p class="text-gray-700">{{ $cartItem->product->prix * $cartItem->quantity }} DA</p>
+                            </div>
+                        @endforeach
+
                         <hr class="my-4" />
                         <div class="flex justify-between">
                             <p class="text-lg font-bold">Total</p>
