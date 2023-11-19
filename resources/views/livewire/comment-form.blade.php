c95736f383c160772ed17c227ee8acebfe8c30af
BENZERDJEB MOHAMED Adnane <skillzdev@hotmail.com>
2023-11-19 13:18:30 +0100

    added french lang  support and changed timezone to algiers updated code added more logic for reviews and improved user experience

--- resources/views/livewire/comment-form.blade.php
+++ resources/views/livewire/comment-form.blade.php
@@ -1,12 +1,12 @@
 <div x-data="{ rating: @entangle('rating') }">
     <div class="flex items-center justify-center shadow-lg mt-16 mb-4">
-        <form class="w-full bg-white rounded-lg px-4 pt-2" wire:submit.prevent='store()'>
+        <form class="w-full bg-white rounded-lg px-4 pt-2" wire:submit='store()'>
             <div class="flex flex-wrap -mx-3 mb-6">
                 <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Ajouter une nouvelle évaluation</h2>
                 <div class="w-full px-3 mb-2 mt-2">
                     <input wire:model="title"
                         class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-10 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white mb-4"
-                        name="title" placeholder='Titre' required>
+                        name="title" placeholder='Titre' required />
                     <textarea wire:model="comment"
                         class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
                         name="body" placeholder='Votre évaluation' required></textarea>
@@ -24,6 +24,11 @@
                             @endfor
                         </div>
                         <input id="rating" name="rating" type="hidden" wire:model="rating">
+                        @error('rating')
+                            <div class="text-red-500 mt-2 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="w-full flex items-start md:w-full px-3 mt-2">
