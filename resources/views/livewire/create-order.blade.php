d623086d8451a45ac07549aeae283301862cad3a
skillz <skillzdev@hotmail.com>
2023-12-09 16:51:15 +0100

    fixing a huge order mistake

--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -7,7 +7,7 @@
                 <div class="relative flex flex-col gap-1.5 w-full">
                     <div class="relative flex items-center w-full">
                         <input type="text" id="billing-address" name="adresse" wire:model='adresse'
-                            class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                            class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:border-blue-500 focus:ring-blue-500"
                             placeholder="Adresse" />
                         <div class="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                             <img class="h-4 w-6 border" src="{{ asset('assets/algeria.png') }}" />
