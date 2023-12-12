b4f1b98d0d6023c2d11dced59bfbe1f41d8318b0
BENZERDJEB MOHAMED Adnane <skillzdev@hotmail.com>
2023-12-12 20:02:42 +0100

    editing controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use Invoice;
+use LaravelDaily\Invoices\Classes\Buyer;
+use LaravelDaily\Invoices\Classes\InvoiceItem;
+use LaravelDaily\Invoices\Classes\Party;
 
 class UserController extends Controller
 {
@@ -45,19 +49,12 @@ class UserController extends Controller
             "commune"
         )->where("buyer_id", auth()->user()->id);
 
-        // Filtering by search term of buyer name
+        // Filtering by search term of artisan name
         if ($request->has("search")) {
             $query->whereHas("artisan", function ($query) {
                 $query
                     ->where("nom", "like", "%" . request("search") . "%")
-                    ->orWhere("prenom", "like", "%" . request("search") . "%")
-                    ->orWhere("email", "like", "%" . request("search") . "%")
-                    ->orWhere("phone", "like", "%" . request("search") . "%")
-                    ->orWhere(
-                        'CONCAT(nom, " ", prenom)',
-                        "like",
-                        "%" . request("search") . "%"
-                    );
+                    ->orWhere("prenom", "like", "%" . request("search") . "%");
             });
         }
 
@@ -83,5 +80,52 @@ class UserController extends Controller
         return view("user.orders.show", [
             "order" => $order,
         ]);
+    }
+
+    public function generateInvoice(Order $order)
+    {
+        $customer = new Buyer([
+            "name" => $order->buyer->getFullName(),
+            "custom_fields" => [
+                "Adresse" => $order->adresse,
+                "Wilaya" => $order->wilaya,
+                "Daira" => $order->daira,
+                "Commune" => $order->commune,
+                "Numéro de Telephone" => $order->buyer->num_telephone,
+                "Email" => $order->buyer->email,
+            ],
+        ]);
+
+        $artisan = new Party([
+            "name" => $order->artisan->getFullName(),
+            "custom_fields" => [
+                "Adresse" => $order->artisan->adresse,
+                "Wilaya" => $order->artisan->wilaya,
+                "Numéro de Telephone" => $order->artisan->num_telephone,
+                "Email" => $order->artisan->email,
+            ],
+        ]);
+
+        $items = $order->orderItems->map(function ($orderItem) {
+            return InvoiceItem::make($orderItem->product->nom)
+                ->pricePerUnit($orderItem->product->prix)
+                ->quantity($orderItem->quantity);
+        });
+
+        $invoice = Invoice::make()
+            ->seller($artisan)
+            ->buyer($customer)
+            ->addItems($items)
+            ->dateFormat("d/m/Y")
+            ->currencySymbol("DA")
+            ->currencyCode("DZD")
+            ->currencyFormat("{VALUE} {SYMBOL}")
+            ->logo(public_path("assets\LOGO.png"))
+            ->filename(
+                "invoice_" . $order->id . "_" . $order->buyer->getFullName()
+            )
+            ->save("public");
+
+        return $invoice->stream();
     }
 }
