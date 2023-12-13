2070168ff23e0a5577cd00d9952e40723938ae1c
skillz <skillzdev@hotmail.com>
2023-12-13 16:04:43 +0100

    added ide helper comments

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,39 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Order
+ *
+ * @property int $id
+ * @property int $buyer_id
+ * @property int $artisan_id
+ * @property string $adresse
+ * @property string $wilaya
+ * @property string $daira
+ * @property string $commune
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $artisan
+ * @property-read \App\Models\User $buyer
+ * @property-read \App\Models\Delivery|null $delivery
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\OrderItem> $orderItems
+ * @property-read int|null $order_items_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereAdresse($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereArtisanId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereBuyerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCommune($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereDaira($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereWilaya($value)
+ * @mixin \Eloquent
+ */
 class Order extends Model
 {
     use HasFactory;
